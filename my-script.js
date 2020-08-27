9e10349ba77db4126d53521a75b0e1e2a24c8940
Sajal3232 <sajalhossain3232@gmail.com>
2020-08-27 13:31:00 +0600

    select option

--- my-script.js
+++ my-script.js
@@ -55,12 +55,71 @@ else
 }
 }
 
-$("#inputEmail1").click(function(){
-    checkEmail();
-})
+// $("#inputEmail1").click(function(){
+//     checkEmail();
+// })
 $("#inputEmail1").blur(function(){
     checkEmail();
 })
 $("#inputEmail1").keyup(function(){
     checkEmail();
 })
+
+// password
+function checkPassword(){
+   var passwordlen= $("#Password1").val().length;
+   if(passwordlen>=6 && passwordlen<=15){
+    $("#Password1Error").text("");
+}
+else{
+    $("#Password1Error").text("password should be 8 to 15 character");
+}
+}
+
+$("#Password1").click(function(){
+    checkPassword();
+})
+$("#Password1").blur(function(){
+    checkPassword();
+})
+$("#Password1").keyup(function(){
+    checkPassword();
+
+})
+
+// confirmPassword1
+function checkconfirmpass(){
+    var oldpass=$("#Password1").val();
+    var confirmpass=$("#confirmPassword1").val();
+    if(oldpass==confirmpass){
+        $("#confirmPassword1Error").text("");
+    }
+    else{
+        $("#confirmPassword1Error").text("Invalid password");
+    }
+}
+
+
+$("#confirmPassword1").blur(function(){
+    checkconfirmpass();
+})
+$("#confirmPassword1").keyup(function(){
+    checkconfirmpass();
+
+})
+
+// district name
+ function districtName(){
+     var district=$("#district-name").val();
+     if(district==" "){
+         alert("test");
+     }
+     else{
+         alert("hello");
+     }
+ }
+
+
+// //  $("#district").click(function(){
+//     districtName();
+// })
